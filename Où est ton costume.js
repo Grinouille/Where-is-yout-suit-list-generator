e3ac1dc4fbf59aeb75bb8553b1f3d9ef5ff2a67b
Grinny <narsilla49@live.fr>
2022-10-08 23:01:31 -0400

    Add files via upload

---
+++ Où est ton costume.js
@@ -0,0 +1,387 @@
+﻿var list = [
+{
+    personnage: "Chaourcette",
+	auteur: "Chaourcette",
+	IDauteur: "1",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Mamie Molette",
+	auteur: "Chaourcette",
+	IDauteur: "1",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Vampire Chaourcette",
+	auteur: "Chaourcette",
+	IDauteur: "1",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Corvus",
+	auteur: "Corvus",
+	IDauteur: "2",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Loki",
+	auteur: "Corvus",
+	IDauteur: "2",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Cray",
+	auteur: "Cray",
+	IDauteur: "3",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Delfine ",
+	auteur: "Delfine ",
+	IDauteur: "4",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Elexa",
+	auteur: "Elexa",
+	IDauteur: "5",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Gouape",
+	auteur: "Gouape",
+	IDauteur: "6",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Grinouille",
+	auteur: "Froppy",
+	IDauteur: "7",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Klariss",
+	auteur: "Froppy",
+	IDauteur: "7",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Marguerite",
+	auteur: "Froppy",
+	IDauteur: "7",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Beau Jacques",
+	auteur: "Froppy",
+	IDauteur: "7",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Mc Nugget",
+	auteur: "Froppy",
+	IDauteur: "7",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Grinny",
+	auteur: "Froppy",
+	IDauteur: "7",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "HadH",
+	auteur: "HadH",
+	IDauteur: "8",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Hazukazoo",
+	auteur: "Hazukazoo",
+	IDauteur: "9",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Hiyokose",
+	auteur: "Hiyokose",
+	IDauteur: "10",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Hariko",
+	auteur: "Hiyokose",
+	IDauteur: "10",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Kose",
+	auteur: "Hiyokose",
+	IDauteur: "10",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Johnny",
+	auteur: "Johnny",
+	IDauteur: "11",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Phil",
+	auteur: "Johnny",
+	IDauteur: "11",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Krom",
+	auteur: "Johnny",
+	IDauteur: "11",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Kaladel (oni)",
+	auteur: "Kaladel (oni)",
+	IDauteur: "12",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Dragon Cosmique",
+	auteur: "Kaladel (oni)",
+	IDauteur: "12",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Yokaïladel",
+	auteur: "Kaladel (oni)",
+	IDauteur: "12",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Loon",
+	auteur: "Loon",
+	IDauteur: "13",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Jaegerius",
+	auteur: "Loon",
+	IDauteur: "13",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "M. Tips",
+	auteur: "M. Tips",
+	IDauteur: "14",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Afrya",
+	auteur: "M. Tips",
+	IDauteur: "14",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Manio",
+	auteur: "Manio",
+	IDauteur: "15",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Mirana",
+	auteur: "Mirana",
+	IDauteur: "16",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Oriu",
+	auteur: "Oriu",
+	IDauteur: "17",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Perline",
+	auteur: "Perline",
+	IDauteur: "18",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Togo",
+	auteur: "Perline",
+	IDauteur: "18",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Izaro",
+	auteur: "Perline",
+	IDauteur: "18",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Prunelle",
+	auteur: "Prunelle",
+	IDauteur: "19",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Rødkløver",
+	auteur: "Rødkløver",
+	IDauteur: "20",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Sereil",
+	auteur: "Sereil",
+	IDauteur: "21",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Stars E",
+	auteur: "Stars E",
+	IDauteur: "22",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Toonuki",
+	auteur: "Toonuki",
+	IDauteur: "23",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Croknuki",
+	auteur: "Toonuki",
+	IDauteur: "23",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Dranuki",
+	auteur: "Toonuki",
+	IDauteur: "23",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Tynt",
+	auteur: "Tynt",
+	IDauteur: "24",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Khlar",
+	auteur: "Tynt",
+	IDauteur: "24",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Urashi",
+	auteur: "Urashi",
+	IDauteur: "25",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Patatashi",
+	auteur: "Urashi",
+	IDauteur: "25",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Hana",
+	auteur: "Urashi",
+	IDauteur: "25",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Zy",
+	auteur: "Zy",
+	IDauteur: "26",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Aëlia",
+	auteur: "Aëlia",
+	IDauteur: "27",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Ïlun",
+	auteur: "Aëlia",
+	IDauteur: "27",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Ceo",
+	auteur: "Ceo",
+	IDauteur: "28",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Ilona",
+	auteur: "Ceo",
+	IDauteur: "28",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Cernunnos",
+	auteur: "Ceo",
+	IDauteur: "28",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Tonio",
+	auteur: "Ceo",
+	IDauteur: "28",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Daireen",
+	auteur: "Ceo",
+	IDauteur: "28",
+    theme: "",
+    contrainte: ""
+},{
+    personnage: "Kooldesource",
+	auteur: "Ceo",
+	IDauteur: "28",
+    theme: "",
+    contrainte: ""
+},
+];
+
+
+
+function swap(a, i, j) {
+    "use strict";
+    var b = a[i];
+    a[i] = a[j];
+    a[j] = b;
+}
+
+function secretsanta() {
+    // Shuffle
+    for (var i = 0; i < list.length;i += 1) {
+        var j = parseInt(Math.random() * list.length);
+        for (var i = 0;list[i].IDauteur != list[j].IDauteur;i += 1){swap(list, i, j)}
+    }
+
+    // Print
+    //console.log(list);
+    var str1 = "Bonjour lubien.ne connu sous le nom de ";
+    var str2 = ", le personnage en lequel tu devras te déguiser sera ";
+    var str3 = ", personnage de ";
+	var str4 = "<br><br>Costumièrement,<br><br> Perline Jolicoeur.";
+    
+    for (var i = 0; i < list.length; i += 1) {
+        j = i + 1;
+        if (j == list.length) {
+            j = 0;
+        }
+        
+        a = document.createElement("div");
+        a.innerHTML = str1 + "<b>" + list[i].auteur + "</b>"  + str2 + "<b>" + list[j].personnage + "</b>" + str3+ "<b>" + list[j].auteur + "</b>" + str4;
+        document.body.appendChild(a);
+        document.body.appendChild(document.createElement("br"));
+    }
+}
+function verificationtirage(){
+    if(list[i].IDauteur = list[j].IDauteur){
+        result="erreur trouvée";
+    } else {result="aucune erreure trouvée"}
+    }
+console.log (verificationtirage())
